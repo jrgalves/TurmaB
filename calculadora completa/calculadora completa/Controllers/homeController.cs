0896c4cd0089cae63c9b6fc682df1c63de9a838a
jrgalves <joaorga1@hotmail.com>
2017-03-24 16:58:58 +0000

    fase inicial do interface da calculadora

---
+++ calculadora completa/calculadora completa/Controllers/homeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace calculadora_completa.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // POST: home
+        [HttpPost]
+        public ActionResult Index(string bt)
+        {
+            //determinar qual o conteudo do botao
+            return View();
+        }
+    }
+}
