9d411e6d2911f1a74f5256f2998253fd3a022dd9
jrgalves <joaorga1@hotmail.com>
2017-03-28 12:19:55 +0100

    reconhecimento dos algarismos

--- calculadora completa/calculadora completa/Controllers/homeController.cs
+++ calculadora completa/calculadora completa/Controllers/homeController.cs
@@ -15,8 +15,27 @@ namespace calculadora_completa.Controllers
         }
         // POST: home
         [HttpPost]
-        public ActionResult Index(string bt)
+        public ActionResult Index(string bt,string visor)
         {
+            switch (bt) {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    //determinar se no visor so existe zero
+                    if (visor.Equals("0")) visor = bt;
+                    else visor += bt;
+                    //visor =visor+bt
+                    break;            
+            }
+            //entregar os valores a view
+            ViewBag.Visor = visor;    
             //determinar qual o conteudo do botao
             return View();
         }
